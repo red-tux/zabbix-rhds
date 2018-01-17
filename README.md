96d13e8bbafbd73a85b85255f823fd48a1fe02a5
red-tux <nelsonab@red-tux.net>
2018-01-17 15:33:16 -0500

    Updated README Updated config.py-default Added meta field to output data.

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Red Hat Directory Server monitoring with ldap.
 
 ## Requirements
 * python-ldap
+* python-dateutil
 
 ## Installation
 To install copy the files into a directory of your choosing.  Next copy the config.py-default to config.py.  Then edit the config.py file with the appropriate information.
--- get_data.py
+++ get_data.py
@@ -3,33 +3,28 @@
 import ldap
 import json
 import pprint
-import sys
+#import sys
 import re
+import datetime
+from dateutil.tz import tzlocal
 
 import config as cfg
 
-reload(sys)
-sys.setdefaultencoding("ISO-8859-1")
+#reload(sys)
+#sys.setdefaultencoding("ISO-8859-1")
 
 pp = pprint.PrettyPrinter(indent=2)
 
-
-try:
-  l = ldap.initialize(cfg.uri)
-  l.protocol_version=ldap.VERSION3
-  l.simple_bind(cfg.username, cfg.password)
-except ldap.LDAPError, e:
-  print e
-
-baseDN = "cn=config"
-searchScope = ldap.SCOPE_SUBTREE
-retrieveAttributes = None
-searchFilter = "(|(cn=features)(cn=monitor)(cn=snmp)(cn=replication))"
-#searchFilter = "cn=snmp"
-
 def get_ldap_data():
   try:
-    ldap_result_id = l.search(baseDN, searchScope, searchFilter) #, retrieveAttributes)
+    l = ldap.initialize(cfg.uri)
+    l.protocol_version=ldap.VERSION3
+    l.simple_bind(cfg.username, cfg.password)
+  except ldap.LDAPError, e:
+    print e
+
+  try:
+    ldap_result_id = l.search(cfg.baseDN, cfg.searchScope, cfg.searchFilter, cfg.retrieveAttributes)
     result_set = {}
     while 1:
       result_type, result_data = l.result(ldap_result_id,0)
@@ -49,6 +44,14 @@ def get_ldap_data():
             result_set[dn]=val
   except ldap.LDAPError, e:
     print e
+  now=datetime.datetime.now(tzlocal())
+  result_set["_meta"]={
+    "uri":cfg.uri,
+    "bindDN":cfg.username,
+    "searchFilter":cfg.searchFilter,
+    "baseDN":cfg.baseDN,
+    "timeStamp":now.strftime("%Y-%m-%d %H:%M:%S %Z")
+  }
   return result_set
 
 
