714c6ca6ae31fe0652e05e25e5f54911f893d27e
A Nelson <nelsonab@rhds10.local>
2019-07-17 10:23:13 -0400

    Merged discovery into get_data via "-d" command line option

--- get_data.py
+++ get_data.py
@@ -8,6 +8,7 @@ import sys
 import re
 import datetime
 import yaml
+import argparse
 from dateutil.tz import tzlocal
 
 # With the YAML config file, I need to still "fake" the old python based config file
@@ -118,5 +119,61 @@ def get_and_group():
     result_set[k]=group_dbordinals(result_set[k])
   return result_set
 
+def get_discovery():
+  result_set={"data":[]}
+
+  # Setup an ldap query to get cn=monitor to find out all the database back ends
+  monitor_dn={"cn=monitor": {
+                "searchScope" : ldap.SCOPE_SUBTREE,
+                "retrieveAttributes" : None,
+                "searchFilter" : "ObjectClass=*"}}
+  monitor_data=get_ldap_data(dns=monitor_dn)
+  backend_dns={}
+
+  # loop through the backends found and set up anothery query for each of the db backends
+  for backend in monitor_data["cn=monitor"]["backendmonitordn"]:
+    backend_dns[backend]={"searchScope" : ldap.SCOPE_SUBTREE,
+                          "retrieveAttributes" : None,
+                          "searchFilter" : "ObjectClass=*"}
+
+  data_set=get_ldap_data(dns=backend_dns)
+
+  # parse out the ordinal data in the result set
+  for k in data_set:
+    data_set[k]=group_dbordinals(data_set[k])
+
+  sub_name_regex=re.compile(r".*\/(.*)")
+  
+  # loop through the backends and build the discovery macros
+  for dn in backend_dns:
+    for k,v in data_set[dn]["db"].items():
+      r=sub_name_regex.match(k)
+      dbfile=r.group(1)
+      group=re.sub("/%s" % (dbfile),"", k)
+
+      #BASEJSON macro makes it easier to directly reference this backend.
+      base_json="['%s'].db['%s']" % (dn, k)
+      i = {"{#DBNAME}": k, 
+           "{#DBGROUP}":group,
+           "{#DBFILE}": dbfile,
+           "{#DBORDINAL}":v["ordinal"],
+           "{#BASEJSON}":base_json}
+      result_set["data"].append(i)
+
+  return result_set
+
 if __name__ == "__main__":
-  print json.dumps(get_and_group(), indent=1)
+  parser = argparse.ArgumentParser(description='Wrapper for Zabbix 2.4')
+  parser.add_argument('-d',action='store_true', help='Show Discovery data')
+
+  try:
+    args=parser.parse_args()
+  except SystemExit as err:
+    if err.code == 2: parser.print_help()
+    sys.exit(err.code)
+
+  if args.d:
+    print(json.dumps(get_discovery(), indent=1))
+  else:
+    print(json.dumps(get_and_group(), indent=1))
+
